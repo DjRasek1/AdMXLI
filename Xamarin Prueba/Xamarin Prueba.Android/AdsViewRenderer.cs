7887f68b95a9bbbbd873b4f6508ab801a4eca3f9
CESAR ELLIOTH ESCOBOZA PEREZ <rasekwashere@hotmail.com>
2022-08-19 12:00:15 -0700

    1era version funcional de la App con linkmode de "none" a "Sdk Only" y package a formato "aab"

---
+++ Xamarin Prueba/Xamarin Prueba.Android/AdsViewRenderer.cs
@@ -0,0 +1,51 @@
+﻿using Android.Content;
+using Android.Gms.Ads;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin_Prueba.viewModel;
+using Xamarin_Prueba.views;
+
+[assembly: ExportRenderer(typeof(AdsView), typeof(AdsViewRenderer))]
+namespace Xamarin_Prueba.viewModel
+{
+    public class AdsViewRenderer : ViewRenderer<AdsView, AdView>
+    {
+        public AdsViewRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<AdsView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null && Control == null)
+                SetNativeControl(CreateAdView());
+
+            /*var adView = new AdView(Context);
+            adView.AdUnitId = "ca-app-pub-3940256099942544/6300978111";
+            var requestBuilder = new AdRequest.Builder();
+            adView.LoadAd(requestBuilder.Build());
+            SetNativeControl(adView);*/
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (e.PropertyName == nameof(AdView.AdUnitId))
+                Control.AdUnitId = Element.AdUnitId;
+        }
+
+        private AdView CreateAdView()
+        {
+            var adView = new AdView(Context)
+            {
+                AdSize = AdSize.Banner,
+                AdUnitId = Element.AdUnitId
+            };
+
+            return adView;
+        }
+    }
+}
---
+++ Xamarin Prueba/Xamarin Prueba/views/adsView.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace Xamarin_Prueba.views
+{
+    public class AdsView : View
+    {
+        public static readonly BindableProperty AdUnitIdProperty = BindableProperty.Create(
+            nameof(AdUnitId), typeof(string),
+            typeof(AdsView), string.Empty);
+
+        public string AdUnitId
+        {
+            get => (string)GetValue(AdUnitIdProperty);
+            set => SetValue(AdUnitIdProperty, value);
+        }
+    }
+}
